97397b4879c47f7db744f87ef6cd12abeb5f8af6
Mario Moran <mjosuemoran029@gmail.com>
2026-02-20 09:08:22 -0600

    Avance 1.1 de la clase RentItem

--- Examen_Programacion2/src/main/java/com/mycompany/examen_programacion2/RentItem.java
+++ Examen_Programacion2/src/main/java/com/mycompany/examen_programacion2/RentItem.java
@@ -3,14 +3,57 @@ package com.mycompany.examen_programacion2;
 import javax.swing.ImageIcon;
 
 
-public class RentItem {
+public abstract class RentItem {
     
     private String nombre;
-    private int codigo;
+    private String codigo;
     private double precioB;
     private int copiasD;
     private ImageIcon imagen; 
     
+    public RentItem(String codigo,String nombre, double precioB){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precioB = precioB;
+        this.copiasD = 0;
+    }
     
+    public abstract double pagoRenta(int dias);
+    
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public double getPrecioBaseRenta(){
+        return precioB;
+    }
+
+    public int getCopiasDisponibles(){
+        return copiasD;
+    }
+
+    public ImageIcon getImagen(){
+        return imagen;
+    }
+    
+    
+    public void setCopiasDisponibles(int copiasD){
+        this.copiasD=copiasD;
+    }
+    
+    public void setImagen(ImageIcon imagen){
+        this.imagen=imagen;
+    }
+    
+    
+    
+    @Override
+    public String toString(){
+        return "Codigo: "+codigo+", Nombre: "+nombre+", Precio Base: "+precioB+", Copias: "+copiasD;
+    }
     
 }
