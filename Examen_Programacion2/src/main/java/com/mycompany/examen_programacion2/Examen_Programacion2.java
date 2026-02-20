1695d056e4dd5bab67f2626953bf8f7c29c28e13
Mario Moran <mjosuemoran029@gmail.com>
2026-02-20 08:37:45 -0600

    Examen

---
+++ Examen_Programacion2/src/main/java/com/mycompany/examen_programacion2/Examen_Programacion2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_programacion2;
+
+/**
+ *
+ * @author mjosu
+ */
+public class Examen_Programacion2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
